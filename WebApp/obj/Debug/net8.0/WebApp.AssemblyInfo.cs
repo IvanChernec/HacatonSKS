55f27f50c79bb7bea7a88b3d9d1944b1b78790c7
ZERRORO <nikitazerorro@gmail.com>
2024-12-04 21:48:43 +0300

    Дай бог поможет мне исправить GitHub

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13d0ecaf163b9a9a36f5dce5b13b945ddbcc41f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c886744bf7573da59de4134a02ab03b393e3c4bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
