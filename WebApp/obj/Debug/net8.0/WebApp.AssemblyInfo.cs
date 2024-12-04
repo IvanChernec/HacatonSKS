49aaceaa0af74f92763c92e9666b651e52a2b195
ZERRORO <nikitazerorro@gmail.com>
2024-12-05 00:05:45 +0300

    Не всё сразу отправилось. Дай бог помощь починить гит

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38d1563b0a196573613a7f304c3bb97d4815bc97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802696f3d73e5938aae6e91f3d89e4b95cb91c06")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
